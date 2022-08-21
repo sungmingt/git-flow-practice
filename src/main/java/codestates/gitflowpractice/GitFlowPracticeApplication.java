fab644477ed3d2665e55ef6efe1be2b6ba223b11
sungmingt <qotjdals8313@naver.com>
2022-08-21 22:38:44 +0900

    first commit

---
+++ src/main/java/codestates/gitflowpractice/GitFlowPracticeApplication.java
@@ -0,0 +1,13 @@
+package codestates.gitflowpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFlowPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codestates/gitflowpractice/GitFlowPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package codestates.gitflowpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
